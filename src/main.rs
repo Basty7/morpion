e1cf8d846d2828e7602a17d5ce2ddf99ea9f1586
Bastien Boutteau <bastien.boutteau@gmail.com>
2024-06-17 01:16:28 +0200

    Added support for my own font !

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
 use eframe::egui;
+use egui::{FontId, RichText, Color32, FontFamily};
 use egui_extras;
 
+
+// Main function obviously ;)
 fn main() {
-    println!("Hello, world!");
+    // Window options: default (size, resizable, etc)
     let win_options = eframe::NativeOptions::default();
+    // Run the app
     let _ = eframe::run_native(
         "My app",
         win_options,
         Box::new(|cc| {
             egui_extras::install_image_loaders(&cc.egui_ctx);
 
-            Box::<Myapp>::default() }
+            // Load fonts
+            // Start with the default fonts (we will be adding to them rather than replacing them).
+            let mut fonts = egui::FontDefinitions::default();
+
+            // Install my own font (maybe supporting non-latin characters).
+            // .ttf and .otf files supported.
+            fonts.font_data.insert(
+                "roboto".to_owned(),
+                egui::FontData::from_static(include_bytes!("../assets/Roboto-Regular.ttf")),
+            );
+
+            fonts.families.insert(
+                FontFamily::Name("roboto".into()),
+                vec!["roboto".to_owned(), "sans-serif".to_owned()],
+            );
+
+
+            cc.egui_ctx.set_fonts(fonts);
+            // Create the app
+            Box::<Myapp>::default()
+        }
         ),
     );
         
 }
 
+//
 struct Myapp {
     name: String,
     age: u32,
@@ -26,13 +51,12 @@ impl Default for Myapp {
         Self {
             name: "John".to_owned(),
             age: 32,
-        }        
+        }
     }
 }
 
 
 impl eframe::App for Myapp {
-
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("My egui Application");
@@ -45,8 +69,11 @@ impl eframe::App for Myapp {
             if ui.button("Increment").clicked() {
                 self.age += 1;
             }
-            ui.label(format!("Hello {}, aged {}", self.name, self.age));
 
+            ui.label(format!("Hello {}, aged {}", self.name, self.age));
+            // ui.label(RichText::new("This is a simple egui application").font(FontId::proportional(10.0)));
+            ui.label(RichText::new("This is a simple egui application").font(FontId::new(20.0, FontFamily::Name ("roboto".into()))));
+            ui.label(RichText::new("This is a simple egui application").color(Color32::RED));
         });
     }
 }
