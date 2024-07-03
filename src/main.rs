8f3ca8e45c3313dd1dcfda039cf838ccc64ba36c
Bastien Boutteau <bastien.boutteau@gmail.com>
2024-07-03 14:26:12 +0200

    Handled end of the game + Way to restart the game + added App icon 2 Players version DONE!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use eframe::egui;
 use egui::{FontFamily, Vec2};
-// use egui::{Color32, FontFamily, FontId, Image, RichText, Vec2};
 use egui_extras;
+use image;
 
-// TODO: Check if someone won the game
-// TODO: Handle the case where the game is a draw
-// TODO: Handle the end of the game
-// DONE: Add a way to restart the game 
+// DONE: Check if someone won the game
+// DONE: Handle the case where the game is a draw
+// DONE: Handle the end of the game
+// DONE: Add a way to restart the game
+// DONE: Add a warning when a player tries to play on a non-empty case
+// DOING: Add an icon to the window
 // TODO: (later) Add a way to play against the computer
 
 fn add_fonts(ctx: &egui::Context) {
@@ -28,12 +30,35 @@ fn add_fonts(ctx: &egui::Context) {
     ctx.set_fonts(fonts);
 }
 
+fn load_icon() -> egui::IconData {
+	let (icon_rgba, icon_width, icon_height) = {
+		let icon = include_bytes!("../assets/icon.png");
+		let image = image::load_from_memory(icon)
+			.expect("Failed to open icon path")
+			.into_rgba8();
+		let (width, height) = image.dimensions();
+		let rgba = image.into_raw();
+		(rgba, width, height)
+	};
+	
+	egui::IconData {
+		rgba: icon_rgba,
+		width: icon_width,
+		height: icon_height,
+	}
+}
+
 // Main function obviously ;)
 fn main() {
     // Window options: default (size, resizable, etc)
-    let mut win_option = eframe::NativeOptions::default();
+    
+    let mut win_option: eframe::NativeOptions = eframe::NativeOptions {
+
+        ..Default::default()
+    };
     // Set the window size
     win_option.viewport.inner_size = Some(Vec2::new(500.0, 400.0));
+    win_option.viewport.icon = Some(load_icon().into());
 
     // Run the app
     let _ = eframe::run_native(
@@ -53,6 +78,7 @@ struct Myapp {
     board: GameBoard,
     turn: bool,
     show_warning: bool,
+    ended:bool,
 }
 
 impl Default for Myapp {
@@ -61,6 +87,7 @@ impl Default for Myapp {
             board: GameBoard::default(),
             turn: false,
             show_warning: false,
+            ended:false,
         }
     }
 }
@@ -71,7 +98,6 @@ struct GameCase {
     // 0 1 2
     // 3 4 5
     // 6 7 8
-    pos: Vec2,
     player: i32,
 }
 
@@ -82,9 +108,8 @@ struct GameBoard {
 impl Default for GameBoard {
     fn default() -> Self {
         let mut cases = Vec::new();
-        for i in 0..9 {
+        for _i in 0..9 {
             cases.push(GameCase {
-                pos: Vec2::new((i % 3) as f32, (i / 3) as f32),
                 player: 0,
             });
         }
@@ -92,7 +117,7 @@ impl Default for GameBoard {
     }
 }
 
-fn draw_grid(ui: &mut egui::Ui, board: &mut GameBoard, turn: &mut bool, show_warning: &mut bool) {
+fn draw_grid(ui: &mut egui::Ui, board: &mut GameBoard, turn: &mut bool, show_warning: &mut bool, ended: &mut bool) {
     egui::Grid::new("grid")
         .spacing(Vec2::new(10., 10.))
         .show(ui, |ui| {
@@ -107,7 +132,7 @@ fn draw_grid(ui: &mut egui::Ui, board: &mut GameBoard, turn: &mut bool, show_war
                     };
                     let bouton2 = ui.add_sized(Vec2::new(100.0, 100.0), bouton);
 
-                    if bouton2.clicked() && case.player == 0 {
+                    if bouton2.clicked() && case.player == 0 && !*ended && !*show_warning{
                         case.player = 1 + *turn as i32;
                         *turn = !*turn;
                     } else if bouton2.clicked() && case.player != 0 {
@@ -119,26 +144,71 @@ fn draw_grid(ui: &mut egui::Ui, board: &mut GameBoard, turn: &mut bool, show_war
         });
 }
 
+fn check_game_end(board: &mut GameBoard) -> i32 {
+    // Check if someone won the game
+    // Returns 0 if no one won, 1 if player 1 won, 2 if player 2 won, 3 if it's a draw
+    for j in 1..3 {
+        // Check the rows
+        for i in 0..3 {
+            if board.cases[i * 3].player == j
+                && board.cases[i * 3 + 1].player == j
+                && board.cases[i * 3 + 2].player == j
+            {
+                return j;
+            }
+        }
+        // Check the columns
+        for i in 0..3 {
+            if board.cases[i].player == j
+                && board.cases[i + 3].player == j
+                && board.cases[i + 6].player == j
+            {
+                return j;
+            }
+        }
+        // Check the diagonals
+        if board.cases[0].player == j && board.cases[4].player == j && board.cases[8].player == j {
+            return j;
+        } else if board.cases[2].player == j
+            && board.cases[4].player == j
+            && board.cases[6].player == j
+        {
+            return j;
+        }
+    }
+    // Check if it's a draw
+    let mut draw = true;
+    for i in 0..9 {
+        if board.cases[i].player == 0 {
+            draw = false;
+        }
+    }
+    if draw {
+        return 3;
+    } else {
+        return 0;
+    }
+}
+
 impl eframe::App for Myapp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.heading(format!(
-                    "Jouez au morpion ! \nC'est au tour du joueur {}",
+                    "Play Tic-Tac-Toe \nIt's player {}'s turn.",
                     (self.turn as i32 + 1)
                 ));
                 let play_again = ui.add_sized(
                     [40., 40.],
                     egui::ImageButton::new(egui::include_image!("../assets/R.png")),
-                );
+                ).on_hover_text("Play again");
 
                 if play_again.clicked() {
                     self.board = GameBoard::default();
                     self.turn = false;
                 }
             });
-
-            draw_grid(ui, &mut self.board, &mut self.turn, &mut self.show_warning);
+            draw_grid(ui, &mut self.board, &mut self.turn, &mut self.show_warning, &mut self.ended);
             if self.show_warning {
                 egui::Window::new("Warning")
                     .collapsible(false)
@@ -150,6 +220,36 @@ impl eframe::App for Myapp {
                         }
                     });
             }
+            let winner = check_game_end(&mut self.board);
+            // Perform actions based on the winner
+            if winner == 3 {
+                self.ended = true;
+                egui::Window::new("Draw")
+                    .collapsible(false)
+                    .title_bar(true)
+                    .show(ctx, |ui| {
+                        ui.label("It's a draw!");
+                        if ui.button("Play again").clicked() {
+                            self.board = GameBoard::default();
+                            self.turn = false;
+                            self.ended = false;
+                        }
+                    });
+            } else if winner != 0 {
+                self.ended = true;
+                egui::Window::new("Winner")
+                    .collapsible(false)
+                    .title_bar(true)
+                    .show(ctx, |ui| {
+                        ui.label(format!("Player {} won!", winner));
+                        if ui.button("Play again").clicked() {
+                            self.board = GameBoard::default();
+                            self.turn = false;
+                            self.ended = false;
+                        }
+                    });
+            }
+
             // ui.horizontal(|ui| {
             //     let name_label = ui.label("Your name: ");
             //     ui.text_edit_singleline(&mut self.name)
