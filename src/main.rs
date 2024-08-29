ae931582d34dbec29370902700ccf51e81763aa1
Bastien Boutteau <bastien.boutteau@gmail.com>
2024-08-29 11:50:52 +0200

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use eframe::web_sys;
 // DONE: Add an icon to the window
 // TODO: (later) Add a way to play against the computer
 
+
 fn add_fonts(ctx: &egui::Context) {
     // Load fonts
     // Start with the default fonts (we will be adding to them rather than replacing them).
